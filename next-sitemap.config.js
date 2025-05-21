2a8325530833e7712f9ebde770b1b6bf6774e922
saalihksa <salihmehmetksa@gmail.com>
2025-05-21 13:34:18 +0300

    Site haritası oluşturma ve site URL yapılandırmaları kaldırıldı

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://emlakplus.com',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    additionalSitemaps: [
-      'https://emlakplus.com/server-sitemap.xml',
-    ],
-  },
-  exclude: ['/server-sitemap.xml'],
+  generateRobotsTxt: false,
 }; 
